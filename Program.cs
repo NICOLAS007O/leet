76e7b94f2c40621a682c0d952b6857888cad6149
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-03-14 22:32:14 -0300

    resolvido

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿
+string galinhapintadinha;
+string nomeLeet;
+
+Console.Clear();
+Console.WriteLine("--- escreva uma frase que ela sera Criptografada --- ");
+galinhapintadinha = Console.ReadLine().ToUpper();
+
+nomeLeet = galinhapintadinha
+            .Replace("A", "4")
+            .Replace("E", "3")
+            .Replace("I", "1")
+            .Replace("O", "0")
+            .Replace("T", "7")
+            .Replace("S", "5");
+
+Console.WriteLine(nomeLeet);
+
